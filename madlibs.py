01885188aa73fb0b0f62a1295a59767b318cbc06
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-20 13:05:23 -0700

    added anoth madlib template

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,6 @@
 """A madlib game that compliments its users."""
 
-from random import choice
+from random import choice, sample
 
 from flask import Flask, render_template, request
 
@@ -13,6 +13,10 @@ AWESOMENESS = [
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful',
     'smashing', 'lovely',
 ]
+
+MADLIB_TEMPLATES = ["ml_school.html", "madlib.html",
+]
+
 
 
 @app.route('/')
@@ -35,11 +39,11 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliments = sample(AWESOMENESS, 3)
 
     return render_template("compliment.html",
                            person=player,
-                           compliment=compliment)
+                           compliments=compliments)
 
 
 @app.route('/game')
@@ -62,15 +66,76 @@ def show_madlib():
     """ Shows madlib with user input"""
     person = request.args.get("person")
     color = request.args.get("color")
-    noun = request.args.get("noun")
+    noun1 = request.args.get("noun1")
+    noun2 = request.args.get("noun2")
+    noun3 = request.args.get("noun3")
+    noun4 = request.args.get("noun4")
+    noun5 = request.args.get("noun5")
     adjectives = request.args.getlist("adjective")
+    adjective1 = request.args.get("adjective1")
+    adjective2 = request.args.get("adjective2")
+    adjective3 = request.args.get("adjective3")
+    adjective4 = request.args.get("adjective4")
+    adjective5 = request.args.get("adjective5")
+    propernoun1 = request.args.get("propernoun1")
+    propernoun2 = request.args.get("propernoun2")
+    pluralnoun1 = request.args.get("pluralnoun1")
+    pluralnoun2 = request.args.get("pluralnoun2")
+    celebrity1 = request.args.get("celebrity1")
+    celebrity2 = request.args.get("celebrity2")
+    liquid1 = request.args.get("liquid1")
+    liquid2 = request.args.get("liquid2")
+    num1 = request.args.get("num1")
+    num2 = request.args.get("num2")
+    num3 = request.args.get("num3")
+    num4 = request.args.get("num4")
+    num5 = request.args.get("num5")
+    superad1 = request.args.get("superad1")
+    superad2 = request.args.get("superad2")
+    superad3 = request.args.get("superad3")
+    adverb1 = request.args.get("adverb1")
+    adverb2 = request.args.get("adverb2")
+    adverb3 = request.args.get("adverb3")
+    adverb4 = request.args.get("adverb4")
+    adverb5 = request.args.get("adverb5")
 
-    return render_template("madlib.html",
+    chosen_madlib = choice(MADLIB_TEMPLATES)
+
+    return render_template(chosen_madlib,
                            person=person,
                            color=color,
-                           noun=noun,
-                           adjectives=adjectives)
-
+                           adjectives=adjectives,
+                           noun1=noun1,
+                           noun2=noun2,
+                           noun3=noun3,
+                           noun4=noun4,
+                           noun5=noun5,
+                           adjective1=adjective1,
+                           adjective2=adjective2,
+                           adjective3=adjective3,
+                           adjective4=adjective4,
+                           adjective5=adjective5,
+                           propernoun1=propernoun1,
+                           propernoun2=propernoun2,
+                           pluralnoun1=pluralnoun1,
+                           pluralnoun2=pluralnoun2,
+                           celebrity1=celebrity1,
+                           celebrity2=celebrity2,
+                           liquid1=liquid1,
+                           liquid2=liquid2,
+                           num1=num1,
+                           num2=num2,
+                           num3=num3,
+                           num4=num4,
+                           num5=num5,
+                           superad1=superad1,
+                           superad2=superad2,
+                           superad3=superad3,
+                           adverb1=adverb1,
+                           adverb2=adverb2,
+                           adverb3=adverb3,
+                           adverb4=adverb4,
+                           adverb5=adverb5,)
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
